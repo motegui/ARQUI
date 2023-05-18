681611f16b0cc1a228c92ff785da5e664e3f4735
motegui <80490650+motegui@users.noreply.github.com>
2023-05-18 12:54:37 -0300

    implementacion del video_driver

---
+++ Kernel/include/video.h
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#define BLACK 0x000000
+#define GREEN 0x00FF00
+#define RED 0xFF0000
+#define WHITE 0xFFFFFF
+#define CORAL 0xFF7F50
+#define PINK 0xFF69B4
+#define INITIAL_FONT_SIZE 1
+
+#define CHAR_WIDTH 8
+#define CHAR_HEIGHT 16
+
+void putpixel(int x, int y, int color);
+
