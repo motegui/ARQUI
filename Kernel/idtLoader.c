eca184d6210a93917bf4d4711bbad1e55423352c
motegui <80490650+motegui@users.noreply.github.com>
2023-05-18 19:27:34 -0300

    Update idtLoader.c

--- Kernel/idtLoader.c
+++ Kernel/idtLoader.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
 #include <idtLoader.h>
-#include <defs.h>
+#include <sys_calls.h>
 #include <interrupts.h>
-#include <syscalls.h>
+#include <defs.h>
 
 #pragma pack(push)		/* Push de la alineación actual */
 #pragma pack (1) 		/* Alinear las siguiente estructuras a 1 byte */
@@ -24,14 +24,14 @@ static void setup_IDT_entry (int index, uint64_t offset); // index which positio
 
 void load_idt() { // its called at the begining of main to load IDT
   _cli();
-  setup_IDT_entry (0x20, (uint64_t)&_irq00Handler); // adding timer 
+  setup_IDT_entry (0x20, (uint64_t)&_irq00Handler); // adding timer
   setup_IDT_entry(0x21,  (uint64_t)&_irq01Handler); // adding keyboard
   setup_IDT_entry(0x80, (uint64_t)&int80Handler); // Setting for software interruptions
   setup_IDT_entry (0x00, (uint64_t)&_exception0Handler); // /0 exception
   setup_IDT_entry (0x06, (uint64_t)&_exception6Handler); // op code exception
 	picMasterMask(0xFC); //Mask to allow interruptions
 	picSlaveMask(0xFF);
-        
+
 	_sti();
 }
 
