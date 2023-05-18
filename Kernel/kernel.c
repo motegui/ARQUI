b6421921b86085246a6c8c5f3168513977f33de3
motegui <80490650+motegui@users.noreply.github.com>
2023-05-18 15:57:39 -0300

    pintear letras

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -11,7 +11,12 @@
 #define BLACK 0x000000
 #define YELLOW 0xFFFF00
 #define  ORANGE 0xFFA500
-
+void draw_string(int x, int y, char* input, int len, int color, int backgroundColor);
+void draw_char(int x, int y, char letter, int color, int backgroundColor);
+void set_default_fontSize(int size);
+int get_default_fontSize();
+void fillrect(int x, int y, int color, int w, int h);
+int DEFAULT_LETTER_SIZE = 8; // Tamaño de letra predeterminado
 
 extern uint8_t text;
 extern uint8_t rodata;
@@ -59,5 +64,14 @@ int main()
 {
 	ncPrint("[Kernel Main]");
 	// printPixelsInFullScreen(r, g, b);
+	 set_default_fontSize(16); // Establece el tamaño de letra predeterminado a 16
+
+    char text[] = "Hello, World!";
+    int x = 100;
+    int y = 100;
+    int color = 0xFF0000; // Rojo
+    int backgroundColor = 0x000000; // Negro
+
+    draw_string(x, y, text, sizeof(text) - 1, color, backgroundColor);
 	return 0;
 }
