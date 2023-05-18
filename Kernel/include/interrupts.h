b2413931b1e12cfa1f6bca7fd6f48ef16d18b4bf
motegui <80490650+motegui@users.noreply.github.com>
2023-05-18 19:22:20 -0300

    interrupts.h

---
+++ Kernel/include/interrupts.h
@@ -0,0 +1,12 @@
+#ifndef INTERRUPS_H_
+#define INTERRUPS_H_
+
+void _sysCallHandler(int mode, int fd, char * buff, int bytes);
+void _irq00Handler(void);
+void _irq01Handler(void);
+void _irq02Handler(void);
+void _irq03Handler(void);
+void _irq04Handler(void);
+void _irq05Handler(void);
+void int80Handler(void);
+#endif /* INTERRUPS_H_ */
