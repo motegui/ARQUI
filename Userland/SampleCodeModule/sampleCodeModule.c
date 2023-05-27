d024a11f152491a1640eeb72c22cbd7e9a866a10
juanamendonca <jmendonca@itba.edu.ar>
2023-05-27 17:41:54 -0300

    funcionalidad help

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -7,7 +7,19 @@
 #define BUFFER_SIZE 50
 
 
- char buffer[BUFFER_SIZE];
+char buffer[BUFFER_SIZE];
+
+void help(){
+	sys_write("The available commands are:", WHITE);
+	enter();
+	sys_write("HELP: to get the information on the available commands", WHITE);
+	enter();
+	sys_write("TIME: to get the current time", WHITE);
+	enter();
+	sys_write("CLEAR: to clear the terminal", WHITE);
+	enter();
+}
+
 
 void command(char * entry){
 	if(strcmp(buffer, "TIME")==0){
@@ -17,6 +29,9 @@ void command(char * entry){
 	else if(strcmp(buffer, "CLEAR")==0){
 		sys_clear_screen();
 	}
+	else if(strcmp(buffer, "HELP")==0){
+		help();
+	}
 	else{
 		sys_write(buffer, WHITE);
 		enter();
@@ -25,6 +40,9 @@ void command(char * entry){
 
 
 int main() {
+	sys_write("Welcome!", WHITE);
+	enter();
+	help();
 	sys_write(">", WHITE);
 	while(1){
 		scanf(buffer, BUFFER_SIZE);
