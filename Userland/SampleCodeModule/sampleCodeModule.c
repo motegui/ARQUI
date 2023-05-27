7df7eae25be7fd428acfd390852ac3760cbf8b9b
juanamendonca <jmendonca@itba.edu.ar>
2023-05-27 14:44:25 -0300

    terminal

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -11,7 +11,12 @@
 #define CORAL 0xFF7F50
 #define PINK 0xFF69B4
 
-char * v = (char*)0xB8000 + 79 * 2;
+#define BUFFER_SIZE 50
+
+
+ char buffer[BUFFER_SIZE];
+
+//char * v = (char*)0xB8000 + 79 * 2;
 
 
 // static int var1 = 0;
@@ -28,20 +33,30 @@ int main() {
 	// 	return 0xDEADC0DE;
 
 	// return 0xDEADBEEF;
-	getTime(WHITE);
+	// getTime(WHITE);
 
-	enter();
+	// enter();
 	
-	sys_write("hola", WHITE);
+	// sys_write("hola", WHITE);
 
-	char  letter[2];
+	char letter[2];
 	letter[1]='\0';
-
+	sys_write(">", WHITE);
 	while(1){
-		sys_read(letter);
-		if (letter[0] != -1 && letter[0]!= 0){
-			sys_write(letter, WHITE);
+		int i=0;
+		while(letter[0] != '\n' && i<BUFFER_SIZE-1){
+			sys_read(letter);
+			if (letter[0] != -1 && letter[0]!= 0 && letter[0]!='\n'){
+				sys_write(letter, WHITE);
+				buffer[i++]=letter[0];
+			}
 		}
+		buffer[i]='\0';
+		enter();
+		sys_write(buffer, WHITE);
+		letter[0]=0;
+		enter();
+		sys_write(">", WHITE);
 	}
 
 	return 0;
